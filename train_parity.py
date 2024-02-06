13620dc19749e5e521b3d8f801efcccd582ab4c3
Dashiell Stander <dstander@protonmail.com>
2024-02-05 18:46:08 -0800

    two layers
    Signed-off-by: Dashiell Stander <dstander@protonmail.com>

--- train_parity.py
+++ train_parity.py
@@ -191,7 +191,7 @@ def main(args):
         "act_fn": "relu"
     }
     num_steps = 100_000
-    num_warmup = 5_000
+    num_warmup = 500
     seed = 100
 
     wandb.init(config=cfg, entity='dstander', project='rasp-parities')
@@ -199,7 +199,7 @@ def main(args):
     config = HookedTransformerConfig(**cfg)
     model = HookedTransformer(config)
 
-    optimizer = torch.optim.AdamW(model.parameters(), lr=0.001, weight_decay=0.001)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.0025, weight_decay=0.001)
     #warmup = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=0.001, end_factor=1.0, total_iters=num_warmup)
     #annealing = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=(num_steps - num_warmup), eta_min=1.0e-6)
     #scheduler = torch.optim.lr_scheduler.SequentialLR(optimizer, [warmup, annealing], milestones=[num_warmup])
